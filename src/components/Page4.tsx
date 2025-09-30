e1852bec70853474ceeb874cffd61be9da368839
VEMULA SRINIVAS <22bk1a05h6@stpetershyd.com>
2025-09-30 18:01:12 +0530

    Update Page4.tsx

--- src/components/Page4.tsx
+++ src/components/Page4.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-
-type Props = { onBack: () => void };
+type Props = {
+  onBack: () => void;
+};
 
 function Page4({ onBack }: Props) {
   return (
     <div className="page page4">
-      <h1 className="final-message">💖 I KNOW AM THE ONE WHO'S GONNA SAY THIS, SO YEAH REPUNZEL I FELL IN LOVE WITH YOU. I REALLY REALLY REALLY LOVE YOU FROM THE BOTTOM OF MY HEART AND I WILL BE LOVING YOU FOR THE REST MY LIFE🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂💖</h1>
-      <button className="love-button" onClick={onBack}>⬅ Back</button>
+      <h1 className="final-message">
+        💖 I KNOW I’M THE ONE WHO'S GONNA SAY THIS, SO YEAH REPUNZEL I FELL IN LOVE WITH YOU.  
+        I REALLY, REALLY, REALLY LOVE YOU FROM THE BOTTOM OF MY HEART AND I WILL BE LOVING YOU FOR THE REST OF MY LIFE 🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂🫂💖
+      </h1>
+      <button className="love-button" onClick={onBack}>
+        ⬅ Back
+      </button>
     </div>
   );
 }
