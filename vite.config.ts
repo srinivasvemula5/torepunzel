f77d7e0810dc6eb6208bfb7a3e830df95bdafb4a
VEMULA SRINIVAS <22bk1a05h6@stpetershyd.com>
2025-09-30 18:12:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/torepunzel", // use import.meta.env
+  base: import.meta.env.VITE_BASE_PATH || "/torepunzel",
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
