542a22e078164cb7c0cb36265d499280b3a42694
VEMULA SRINIVAS <22bk1a05h6@stpetershyd.com>
2025-09-30 18:58:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Use env variable if set, otherwise default to "/"
-  base: process.env.VITE_BASE_PATH || "/",
+  base: "/", // ALWAYS use "/" for Vercel root deployment
 });
